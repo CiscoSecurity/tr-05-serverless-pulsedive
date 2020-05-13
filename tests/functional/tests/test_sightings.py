f2f7a3d97cfc0dbe1eaa4ac26b9d3d7af548e013
AnastasiiaRozlyvan <nastia.2.r.2@gmail.com>
2020-05-13 17:34:17 +0300

    Fixed func tests 2

--- tests/functional/tests/test_sightings.py
+++ tests/functional/tests/test_sightings.py
@@ -38,16 +38,16 @@ def test_positive_sighting_domain(module_headers):
 
         assert len(sighting['observables']) == 1
         assert sighting['observables'][0] == observable
-
-        assert len(sighting['relations']) == 2
-        for relation in sighting['relations']:
-            assert relation['origin'] == 'Pulsedive Enrichment Module'
-            assert relation['relation'] == 'Resolved_To'
-            assert relation['source'] == {'value': 'brehmen.com',
-                                          'type': 'domain'}
-            assert relation['related']['value'] in ('81.169.145.159',
-                                                    '2a01:238:20a:202:1159::')
-            assert relation['related']['type'] in ('ip', 'ipv6')
+        if 'relations' in sighting:
+            assert len(sighting['relations']) == 1
+            for relation in sighting['relations']:
+                assert relation['origin'] == 'Pulsedive Enrichment Module'
+                assert relation['relation'] == 'Resolved_To'
+                assert relation['source'] == {'value': 'brehmen.com',
+                                              'type': 'domain'}
+                assert relation['related']['value'] in ('81.169.145.159',
+                                                        '2a01:238:20a:202:1159::')
+                assert relation['related']['type'] in ('ip', 'ipv6')
 
 
 def test_positive_sighting_ip(module_headers):
