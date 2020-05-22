108b1e2a9ec4eacb4f233b02e6c8243ce179d942
Pavlo Havrylov <greenpasha22@gmail.com>
2020-05-22 11:33:39 +0300

    add active dns sighting check

--- tests/functional/tests/test_sightings.py
+++ tests/functional/tests/test_sightings.py
@@ -25,11 +25,14 @@ def test_positive_sighting_domain(module_headers):
     sightings = get_observables(
         response['data'], 'Pulsedive')['data']['sightings']
     assert sightings['count'] == 6
+    active_dns_sighting = 0
 
     for sighting in sightings['docs']:
         assert sighting['count'] == 1
         assert sighting['id'].startswith('transient:sighting-')
         assert sighting['description']
+        if sighting['description'] == 'Active DNS':
+            active_dns_sighting += 1
         assert sighting['confidence'] == 'Medium'
         assert 'start_time' in sighting['observed_time']
         assert sighting['schema_version']
@@ -48,6 +51,7 @@ def test_positive_sighting_domain(module_headers):
                 assert relation['related']['value'] in \
                     ('81.169.145.159', '2a01:238:20a:202:1159::')
                 assert relation['related']['type'] in ('ip', 'ipv6')
+    assert active_dns_sighting == 1
 
 
 def test_positive_sighting_ip(module_headers):
@@ -73,12 +77,15 @@ def test_positive_sighting_ip(module_headers):
     sightings = get_observables(
         response['data'], 'Pulsedive')['data']['sightings']
     assert sightings['count'] == 13
+    active_dns_sighting = 0
 
     for sighting in sightings['docs']:
         assert sighting['count'] == 1
         assert sighting['id'].startswith('transient:sighting-')
 
         assert sighting['description']
+        if sighting['description'] == 'Active DNS':
+            active_dns_sighting += 1
         assert sighting['confidence'] == 'Medium'
 
         assert 'start_time' in sighting['observed_time']
@@ -89,6 +96,7 @@ def test_positive_sighting_ip(module_headers):
 
         assert len(sighting['observables']) == 1
         assert sighting['observables'][0] == observable
+    assert active_dns_sighting == 1
 
 
 def test_positive_sighting_url(module_headers):
