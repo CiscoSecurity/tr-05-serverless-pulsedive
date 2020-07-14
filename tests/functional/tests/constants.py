4be08a6f38be1e84fc16124dbf475d765010fb6d
Pavlo Havrylov <greenpasha22@gmail.com>
2020-07-14 13:45:20 +0300

    fix and improve test_refer

---
+++ tests/functional/tests/constants.py
@@ -0,0 +1,12 @@
+MODULE_NAME = 'Pulsedive'
+PULSEDIVE_URL = 'https://pulsedive.com'
+OBSERVABLE_HUMAN_READABLE_NAME = {
+    'ip': 'IP',
+    'ipv6': 'IPv6',
+    'url': 'URL',
+    'sha256': 'SHA256',
+    'md5': 'MD5',
+    'sha1': 'SHA1',
+    'email': 'email',
+    'domain': 'domain'
+}
--- tests/functional/tests/test_refer.py
+++ tests/functional/tests/test_refer.py
@@ -1,8 +1,26 @@
+import pytest
 from ctrlibrary.core.utils import get_observables
 from ctrlibrary.threatresponse.enrich import enrich_refer_observables
+from tests.functional.tests.constants import (
+    MODULE_NAME,
+    PULSEDIVE_URL,
+    OBSERVABLE_HUMAN_READABLE_NAME
+)
+from urllib.parse import quote
 
 
-def test_positive_refer_observable(module_headers):
+@pytest.mark.parametrize(
+    'observable,observable_type',
+    (
+     ('1.1.1.1', 'ip'),
+     ('brehmen.com', 'domain'),
+     ('2a01:238:20a:202:1159::', 'ipv6'),
+     ('http://juanthradio.com/Script/DOC/', 'url'),
+     ('2a01:238:20a:202:1159::', 'ipv6'),
+     )
+)
+def test_positive_refer_observable(module_headers, observable,
+                                   observable_type):
     """Perform testing for enrich refer observables endpoint to get
     data for observable from Pulsedive
 
@@ -17,21 +35,43 @@ def test_positive_refer_observable(module_headers):
 
     Importance: Critical
     """
-    observable_type = 'ip'
-    observable_value = '1.1.1.1'
-    observables = [{'type': observable_type, 'value': observable_value}]
+    observables = [{'type': observable_type, 'value': observable}]
 
     response = enrich_refer_observables(
         payload=observables,
         **{'headers': module_headers}
     )['data']
 
-    refer = get_observables(response, 'Pulsedive')
+    refers = get_observables(response, MODULE_NAME)
 
-    assert refer['id'] == (
-        f'ref-pulsedive-search-{observable_type}-{observable_value}')
-    assert refer['module'] == 'Pulsedive'
-    assert refer['title'] == 'Search for this IP'
-    assert refer['description'] == 'Lookup this IP on Pulsedive'
-    assert refer['categories'] == ['Pulsedive', 'Search']
-    assert refer['url'].startswith('https://pulsedive.com/browse')
+    for refer in refers:
+        if observable_type == 'ip' or observable_type == 'domain':
+            assert refer['id'].startswith('ref-pulsedive') and (
+                refer['id'].endswith(f'{observable_type}-{observable}'))
+        else:
+            assert refer['id'].startswith('ref-pulsedive') and (
+                refer['id'].endswith(
+                    f'{observable_type}-{quote(observable, safe="")}'))
+        assert refer['module'] == MODULE_NAME
+        assert refer['module_instance_id']
+        assert refer['module_type_id']
+
+        if 'Search' in refer['title']:
+            assert refer['title'] == (
+                f'Search for this '
+                f'{OBSERVABLE_HUMAN_READABLE_NAME[observable_type]}')
+            assert refer['description'] == (
+                f'Lookup this '
+                f'{OBSERVABLE_HUMAN_READABLE_NAME[observable_type]} '
+                f'on {MODULE_NAME}')
+            assert refer['categories'] == [f'{MODULE_NAME}', 'Search']
+            assert refer['url'].startswith(f'{PULSEDIVE_URL}/browse/')
+        else:
+            assert refer['title'] == (
+                f'Browse {OBSERVABLE_HUMAN_READABLE_NAME[observable_type]}')
+            assert refer['description'] == (
+                f'Browse this '
+                f'{OBSERVABLE_HUMAN_READABLE_NAME[observable_type]}'
+                f' on {MODULE_NAME}')
+            assert refer['categories'] == [f'{MODULE_NAME}', 'Browse']
+            assert refer['url'].startswith(f'{PULSEDIVE_URL}/indicator/')
