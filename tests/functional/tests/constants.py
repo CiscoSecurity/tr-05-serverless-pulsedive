b27f95d7be12658d6ff861921b1536251a2bf6f7
Pavlo Havrylov <greenpasha22@gmail.com>
2020-07-14 14:38:34 +0300

    remove redundant constans value

--- tests/functional/tests/constants.py
+++ tests/functional/tests/constants.py
@@ -4,9 +4,5 @@ OBSERVABLE_HUMAN_READABLE_NAME = {
     'ip': 'IP',
     'ipv6': 'IPv6',
     'url': 'URL',
-    'sha256': 'SHA256',
-    'md5': 'MD5',
-    'sha1': 'SHA1',
-    'email': 'email',
     'domain': 'domain'
 }
