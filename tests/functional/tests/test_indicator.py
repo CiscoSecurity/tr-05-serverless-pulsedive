2c13ca13a36096dd3315fd1a09e5be86e299b5ad
Pavlo Havrylov <greenpasha22@gmail.com>
2020-10-29 18:38:52 +0200

    fix commit

--- tests/functional/tests/test_indicator.py
+++ tests/functional/tests/test_indicator.py
@@ -29,7 +29,7 @@ def test_positive_indicator_details(module_headers):
     response_from_all_modules = enrich_observe_observables(
         payload=observables,
         **{'headers': module_headers}
-    )['data']
+    )
     response_from_pulsedive_module = get_observables(
         response_from_all_modules, MODULE_NAME)
 
