09d7245dd8d94382df74f75bc20523910d4c29ce
YevhenLysen <ylysen@softserveinc.com>
2020-11-27 09:07:32 +0200

    Fix autotest by added new IP

--- tests/functional/tests/test_verdict.py
+++ tests/functional/tests/test_verdict.py
@@ -6,7 +6,7 @@ from tests.functional.tests.constants import MODULE_NAME
 
 @pytest.mark.parametrize(
     'observable,observable_type,disposition_name,disposition',
-    (('13.79.174.77', 'ip', 'Clean', 1),
+    (('50.63.202.50', 'ip', 'Clean', 1),
      ('0-100-disc.foxypool.cf', 'domain', 'Malicious', 2),
      ('1.0.3.4', 'ip', 'Suspicious', 3),
      ('98.159.110.20', 'ip', 'Unknown', 5))
