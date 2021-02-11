098a52005bfb0d3f9d5ed97e74bc2c21c3d04d7d
AnastasiiaRozlyvan <48979187+AnastasiiaRozlyvan@users.noreply.github.com>
2021-02-11 18:58:52 +0200

    [CCTRI-2061] Restructure code (#61)
    * [CCTRI-2061] Restructure code
    
    * Add watchdog to app.py, add unit tests, move zappa to code
    
    * Fix README.md
    
    * Fix CommonErrors.md
    
    * Add dot at the end of docker build command
    
    * Remove AWS hosted Relay section

---
+++ code/tests/unit/api/test_watchdog.py
@@ -0,0 +1,23 @@
+from http import HTTPStatus
+from pytest import fixture
+
+
+def routes():
+    yield '/watchdog'
+
+
+@fixture(scope='module', params=routes(), ids=lambda route: f'GET {route}')
+def route(request):
+    return request.param
+
+
+@fixture(scope='module')
+def expected_payload(client):
+    return {'data': 'test'}
+
+
+def test_version_call_success(route, client, expected_payload):
+    response = client.get(route, headers={'Health-Check': 'test'})
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.get_json() == expected_payload
